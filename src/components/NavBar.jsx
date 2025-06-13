50c38ae1932b86eed8e70adc9936f76307b56f9c
MaxiSanchez <maxisanchezms.ms@gmail.com>
2025-06-12 22:22:11 -0300

    Aplicando estilos Bootstrap a NavBar para navegacion responsiva

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,27 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/favoritos">Favoritos</Link></li>
-        <li><Link to="/crear">Crear Producto</Link></li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Mi Tienda</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Inicio</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/favoritos">Favoritos</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/crear">Crear Producto</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
