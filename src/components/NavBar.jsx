15f8099bb887ba47b37f1844018481cea33d9264
MaxiSanchez <maxisanchezms.ms@gmail.com>
2025-06-14 10:00:37 -0300

    Styles: mejorando estilos al navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { FaLaptopCode } from 'react-icons/fa';
+import '../css/NavBar.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <Link className="navbar-brand" to="/">Mi Tienda</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    <nav className="navbar navbar-expand-lg custom-navbar px-4">
+      <NavLink className="navbar-brand d-flex align-items-center" to="/">
+        <FaLaptopCode className="me-2 logo-icon" />
+        <span className="orbitron-font">JUANCA TECNO STORE</span>
+      </NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Inicio</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/favoritos">Favoritos</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/productos/nuevo">Crear Producto</Link>
-            </li>
-          </ul>
-        </div>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => `nav-link custom-navlink ${isActive ? 'active' : ''}`}
+              to="/"
+            >
+              Inicio
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => `nav-link custom-navlink ${isActive ? 'active' : ''}`}
+              to="/favoritos"
+            >
+              Favoritos
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              className={({ isActive }) => `nav-link custom-navlink ${isActive ? 'active' : ''}`}
+              to="/crear"
+            >
+              Crear Producto
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   );
