86485ce8dc4cbc795b695677125eff96b99477b6
Ivo <ivothaielvicencio@gmail.com>
2025-06-21 22:31:48 -0300

    refactor: actualizando el App component para incluir rutas  publicas y  privadas con autenticacion  [ivo thaiel vicencio rosas]

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Provider, useDispatch } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import { store } from "./store/store";
 import NavBar from "./components/NavBar.jsx";
 import Home from "./views/Home.jsx";
@@ -11,6 +11,11 @@ import ProductForm from "./views/ProductForm.jsx";
 import { fetchProducts } from "./store/productSlice";
 import Cart from "./views/Cart.jsx";
 import { Toaster } from "react-hot-toast";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./views/Login"; // para que ya este listo para el que lo cree
+import Register from "./views/Register"; //esto tambien
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../store/authSlice'; //la ruta tiene que ser asi
 
 // Este componente "wrapper" se encargará de la carga inicial de datos y las rutas
 function AppWrapper() {
@@ -27,12 +32,61 @@ function AppWrapper() {
       <main className="flex-grow-1 container py-4">
         {/* Routes debe estar dentro de un Router, que ahora estará en main.jsx */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favoritos" element={<Favorites />} />
-          <Route path="/productos/:id" element={<ProductDetails />} />
-          <Route path="/crear" element={<ProductForm />} />
-          <Route path="/productos/:id/editar" element={<ProductForm />} />
-          <Route path="/carrito" element={<Cart />} />
+          {/* --- Rutas Públicas --- */}
+          {/* Cualquiera puede ver el login y el registro */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* --- Rutas Privadas --- */}
+          {/* Solo usuarios autenticados pueden ver estas rutas */}
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/favoritos"
+            element={
+              <PrivateRoute>
+                <Favorites />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/productos/:id"
+            element={
+              <PrivateRoute>
+                <ProductDetails />
+              </PrivateRoute>
+            }
+          />
+           <Route
+            path="/crear"
+            element={
+              <PrivateRoute>
+                <ProductForm />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/productos/:id/editar"
+            element={
+              <PrivateRoute>
+                <ProductForm />
+              </PrivateRoute>
+            }
+          />
+           <Route
+            path="/carrito"
+            element={
+              <PrivateRoute>
+                <Cart />
+              </PrivateRoute>
+            }
+          />
           {/*<Route path="/acerca" element={<About />} />*/}
         </Routes>
       </main>
