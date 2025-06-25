aa19f1a5b00d98322afc9be8ea7c70d9fdfd8909
Lautaro <barrientoslautaro8@gmail.com>
2025-06-24 23:43:22 -0300

    feat(store): Configuración del store de Redux para autenticación [Barrientos Lautaro]

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productSlice";
 import cartReducer from "./cartSlice";
+import authReducer from "../feactures/auth/authSlice.js"
 
 export const store = configureStore({
-  reducer: { products: productReducer, cart: cartReducer },
-  //Se agregan los reducers para productos y carrito al store
+  reducer: { products: productReducer, cart: cartReducer, auth: authReducer },
+  //Se agregan los reducers para productos, carrito y autenticación al store
 });
