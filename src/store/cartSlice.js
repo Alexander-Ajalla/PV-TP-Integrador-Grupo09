63bf9b69c2bd907769a192cb1f894c33ea1d208a
alex._unu <90481323+Alexander-Ajalla@users.noreply.github.com>
2025-06-26 20:40:26 -0300

    feat: Agregando reducers para aumentar y disminuir cantidad de productos

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -26,6 +26,18 @@ const cartSlice = createSlice({
       }
       state.items = state.items.filter((item) => item.id !== id);
     },
+    increaseQuantity: (state, action) => {
+      const item = state.items.find((item) => item.id === action.payload);
+      if (item) {
+        item.quantity += 1;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const item = state.items.find((item) => item.id === action.payload);
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      }
+    },
     clearCart: (state) => {
       state.items = [];
       state.totalUnits = 0;
