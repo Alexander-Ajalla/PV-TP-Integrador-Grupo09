5c314fdecafe3292ffddafa99e69fdafaa669db5
Ivo <ivothaielvicencio@gmail.com>
2025-06-27 04:12:20 -0300

    feat: Implementar estilos de tema claro y oscuro en la aplicación, incluyendo ajustes en componentes y CSS para una mejor coherencia visual [ivo thaiel vicencio rosas]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,22 @@
 // src/componentes/Footer.jsx
 import '../css/Footer.css';
+import { useSelector } from 'react-redux';
 
 function Footer() {
   const year = new Date().getFullYear();
+  const mode = useSelector((state) => state.theme.mode);
 
   return (
-    <footer className="custom-footer text-white mt-auto py-3">
-      <div className="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+    <footer className={`custom-footer footer-${mode}`}>
+      <div className="footer-content">
         <span>
           &copy; {year} Sistema de Gestión de Productos | Desarrollado por <strong>Grupo 9</strong>
         </span>
-        <div className="social-icons mt-2 mt-md-0">
-          <a href="https://github.com/Alexander-Ajalla/PV-TP-Integrador-Grupo09" target="_blank" rel="noopener noreferrer" className="me-3">
+        <div className="social-icons">
+          <a href="https://github.com/Alexander-Ajalla/PV-TP-Integrador-Grupo09" target="_blank" rel="noopener noreferrer">
             <i className="bi bi-github"></i>
           </a>
-          <a href="https://linkedin.com/in/usuario" target="_blank" rel="noopener noreferrer" className="me-3">
+          <a href="https://linkedin.com/in/usuario" target="_blank" rel="noopener noreferrer">
             <i className="bi bi-linkedin"></i>
           </a>
           <a href="email:email@example.com">
