7ede6d719c46515d0a9859e444ab4249d284bf3d
Ivo <ivothaielvicencio@gmail.com>
2025-06-27 02:32:10 -0300

    feat: Integrar themeSlice en el store para gestionar el tema de la aplicación [ivo thaiel vicencio rosas]

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./productSlice";
 import cartReducer from "./cartSlice";
 import authReducer from "./authSlice";
+import themeSlice from "./themeslice";
+
 
 export const store = configureStore({
   reducer: { 
     products: productReducer, 
     cart: cartReducer, 
-    auth: authReducer 
+    auth: authReducer,
+    theme: themeSlice.reducer
   },
 });
